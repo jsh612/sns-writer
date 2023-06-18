e299d8ef35c88af480ebf02e101403cabf104a04
DEVFATH <ggje33@gmail.com>
2023-06-18 23:29:44 +0900

    인증 모듈 추가, 내용물 추가 필요함

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+
+/**
+ * 1. 티스토리 인증
+ * 2. Access Token 가져오기
+ * 3. 글쓰기
+ * 4. GPT 연결하기
+ * 5. GPT 연결내용 게시글 포맷 맞추기
+ * 6. 관련된 태그 20개 뽑아오기
+ * 7. 글쓰기
+ */
+@Controller('auth')
+export class AuthController {}
